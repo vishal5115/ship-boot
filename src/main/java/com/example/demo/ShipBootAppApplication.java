c15175303a15390ea1f99ddfca4bcd037f5b7395
Vishal <garg.vishal5115@gmail.com>
2021-10-14 16:22:50 +0530

    first commit after initiate project

---
+++ src/main/java/com/example/demo/ShipBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipBootAppApplication.class, args);
+	}
+
+}
